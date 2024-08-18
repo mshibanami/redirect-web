d5378898ef21e54e1f271b0825ec3df48dac235f
Manabu Nakazawa <mshibanami+git@gmail.com>
2024-08-19 04:56:24 +1000

    Run build.sh for GitHub Actions

--- build.sh
+++ build.sh
@@ -3,12 +3,11 @@
 cd "$(dirname "${BASH_SOURCE:-$0}")"
 
 rm -rf output
-mkdir -p output/redirect-web
-cp -R docs/* output/redirect-web
+mkdir -p output/
+cp -R docs/* output/
 
-mkdir -p output/redirect-web/new
+mkdir -p output/new
 cd docusaurus
 npm install
 npm run build
-
-cp -R build/* ../output/redirect-web/new
+cp -R build/* ../output/new
