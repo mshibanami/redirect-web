b13ada9cc5f61cb49e0201688fa6e633f5eba4d7
Manabu Nakazawa <mshibanami+git@gmail.com>
2024-08-19 03:50:50 +1000

    Update the build script to include docusaurus

--- build.sh
+++ build.sh
@@ -5,3 +5,10 @@ cd "$(dirname "${BASH_SOURCE:-$0}")"
 rm -rf output
 mkdir -p output/redirect-web
 cp -R docs/* output/redirect-web
+
+mkdir -p output/redirect-web/new
+cd docusaurus
+npm install
+npm run build
+
+cp -R build/* ../output/redirect-web/new
