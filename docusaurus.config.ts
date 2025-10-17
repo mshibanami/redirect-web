d3d776c805c977b56b94002d00bade5b4d2c05ba
Manabu Nakazawa <mshibanami+git@gmail.com>
2025-10-17 20:13:25 +1100

    translate the tag line.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -4,18 +4,14 @@ import type * as Preset from '@docusaurus/preset-classic';
 import remarkGithubAdmonitionsToDirectives from "remark-github-admonitions-to-directives";
 import remarkCjkFriendly from 'remark-cjk-friendly';
 import { ALL_BROWSERS, getExtensionStore } from './src/components/ExtensionStoreLinks/constants';
-import { translate } from '@docusaurus/Translate';
 
+const locale = process.env.DOCUSAURUS_CURRENT_LOCALE;
 const projectName = 'redirect-web';
 const baseUrl = `/${projectName}/`;
 
 const config: Config = {
   title: 'Redirect Web',
-  tagline: translate({
-    id: 'tagline',
-    description: 'A tagline of the app',
-    message: 'Take Control of Your Web Experience'
-  }),
+  tagline: translatedTagline(locale),
   url: 'https://mshibanami.github.io/',
   baseUrl: baseUrl,
   organizationName: 'mshibanami',
@@ -196,3 +192,66 @@ const config: Config = {
 };
 
 export default config;
+
+function translatedTagline(locale): string {
+  switch (locale) {
+    case 'bg':
+      return 'Оптимизирайте уеб изживяването си';
+    case 'cs':
+      return 'Zefektivněte svůj webový zážitek';
+    case 'da':
+      return 'Strømlin din weboplevelse';
+    case 'de':
+      return 'Optimieren Sie Ihr Weberlebnis';
+    case 'el':
+      return 'Βελτιστοποιήστε την εμπειρία σας στο διαδίκτυο';
+    case 'es':
+      return 'Optimiza tu experiencia web';
+    case 'et':
+      return 'Muuda oma veebikogemus sujuvamaks';
+    case 'fi':
+      return 'Tehosta verkkokokemustasi';
+    case 'fr':
+      return 'Optimisez votre expérience web';
+    case 'hu':
+      return 'Tegye gördülékenyebbé a webes élményét';
+    case 'id':
+      return 'Permudah pengalaman web Anda';
+    case 'it':
+      return 'Ottimizza la tua esperienza web';
+    case 'ja':
+      return 'ウェブ体験をもっとスムーズに';
+    case 'ko':
+      return '웹 경험을 간소화하세요';
+    case 'lt':
+      return 'Supaprastinkite savo žiniatinklio patirtį';
+    case 'lv':
+      return 'Vienkāršojiet savu tīmekļa pieredzi';
+    case 'nl':
+      return 'Stroomlijn uw webervaring';
+    case 'pl':
+      return 'Usprawnij swoje doświadczenie w sieci';
+    case 'pt-br':
+      return 'Otimize sua experiência na web';
+    case 'pt-pt':
+      return 'Otimize a sua experiência na web';
+    case 'ro':
+      return 'Optimizați experiența dvs. pe web';
+    case 'ru':
+      return 'Оптимизируйте ваш веб‑опыт';
+    case 'sk':
+      return 'Zefektívnite svoj webový zážitok';
+    case 'sl':
+      return 'Poenostavite svojo spletno izkušnjo';
+    case 'sv':
+      return 'Förenkla din webbupplevelse';
+    case 'tr':
+      return 'Web deneyiminizi kolaylaştırın';
+    case 'uk':
+      return 'Оптимізуйте свій веб‑досвід';
+    case 'zh-hans':
+      return '优化您的网页体验';
+    default:
+      return 'Streamline Your Web Experience';
+  }
+}
