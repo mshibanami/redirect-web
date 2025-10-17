6e6b7535e02bea8464b36c202330f33e9a509239
Manabu Nakazawa <mshibanami+git@gmail.com>
2025-10-17 17:58:22 +1100

    tutorialSidebar > sidebar

--- sidebars.ts
+++ sidebars.ts
@@ -11,7 +11,7 @@ import type { SidebarsConfig } from '@docusaurus/plugin-content-docs';
  Create as many sidebars as you want.
  */
 const sidebars: SidebarsConfig = {
-  tutorialSidebar: [
+  sidebar: [
     "introduction",
     {
       type: "category",
