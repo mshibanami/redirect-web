4a3400dfe70846e357f91dd16671182c52593821
Manabu Nakazawa <mshibanami+git@gmail.com>
2026-01-09 13:05:46 +1100

    Use dotenv for pnpm dev

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { themes as prismThemes } from 'prism-react-renderer';
 import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
